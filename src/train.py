9da5e51f06c01016f4ca80e1fce64b5700938c01
wirehack <kevinwirehack@gmail.com>
2019-03-24 19:03:33 -0400

    data synthetic

--- src/train.py
+++ src/train.py
@@ -9,9 +9,12 @@ from src.mul_attention import MultiHeadAttention
 from copy import deepcopy
 from src.data_loader import DataLoader
 from src.config import argps
+import data_loader
 
 device = device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-EPOCH_CHECK=1
+EPOCH_CHECK = 10
+
+
 # make the model
 def make_model(src_vocab_size, trg_vocab_size, N=6, d_model=512, d_ff=2048, head_num=8, dropout=0.1):
     attn = MultiHeadAttention(head_num, d_model, dropout)
@@ -30,7 +33,7 @@ def make_model(src_vocab_size, trg_vocab_size, N=6, d_model=512, d_ff=2048, head
     return model
 
 
-def train_epoch(data_iter, model:EncoderDecoder, loss_compute):
+def train_epoch(data_iter, model: EncoderDecoder, loss_compute):
     start_time = time.time()
     tot_tokens = 0
     train_loss = 0
@@ -45,11 +48,13 @@ def train_epoch(data_iter, model:EncoderDecoder, loss_compute):
 
         if (i + 1) % 50 == 0:
             elapsed = time.time() - start_time
-            print("[INFO] in epoch {:d}, loss: {:.4f}, tokens per sec: {:.4f}".format(i, loss / cur_batch.ntokens, tokens / elapsed))
+            print("[INFO] in epoch {:d}, loss: {:.4f}, tokens per sec: {:.4f}".format(i, loss / cur_batch.ntokens,
+                                                                                      tokens / elapsed))
             start_time = time.time()
             tokens = 0
 
     return train_loss / tot_tokens
+
 
 def run_epoch(epoch, data_iter, model, criterion, optimizer=None):
     start_time = time.time()
@@ -68,23 +73,26 @@ def run_epoch(epoch, data_iter, model, criterion, optimizer=None):
         train_loss += loss
         tot_tokens += cur_batch.token_num
         tokens += cur_batch.token_num
-        if (i + 1) % 50 == 0:
+        if i % 50 == 1:
             elapsed = time.time() - start_time
-            print("[INFO] in epoch {:d}, loss: {:.4f}, tokens per sec: {:.4f}".format(i, loss / cur_batch.ntokens,
+            print("[INFO] in epoch {:d}, loss: {:.4f}, tokens per sec: {:.4f}".format(i, loss / cur_batch.token_num,
                                                                                       tokens / elapsed))
             start_time = time.time()
             tokens = 0
     print("[INFO] epoch {:d}: loss={:.1f}/{:.1f}={:.4f}, time={:.2f}".format(epoch,
-                                                                    train_loss, tot_tokens, train_loss / tot_tokens,
-                                                                    time.time() - start_time))
+                                                                             train_loss, tot_tokens,
+                                                                             train_loss / tot_tokens,
+                                                                             time.time() - start_time))
     return train_loss / tot_tokens
 
-def save_model(model:nn.Module, path):
-    torch.save({"model_state_dict":model.state_dict()}, path)
+
+def save_model(model: nn.Module, path):
+    torch.save({"model_state_dict": model.state_dict()}, path)
     print("[SAVE] save model!")
 
+
 def train(args):
-    #TODO add dropout
+    # TODO add dropout
     data_loader = DataLoader(args.train_file, args.dev_file, args.src_suffix, args.trg_suffix,
                              args.w2i_map_file, args.batch_size, args.pool_size, pad=0)
     src_vocab_size, trg_vocab_size = data_loader.src_vocab_size, data_loader.trg_vocab_size
@@ -108,6 +116,39 @@ def train(args):
                     save_model(model, args.model_path + "_" + str(ep) + ".tar")
 
 
+def data_gen(batch, nbatches):
+    "Generate random data for a src-tgt copy task."
+    V = 11
+    for i in range(nbatches):
+        data = torch.from_numpy(np.random.randint(1, V, size=(batch, 10)))
+        data[:, 0] = 1
+        src = data
+        tgt = data
+        yield (data_loader.Batch(src, tgt, 0))
+
+
+def train_synthetic():
+    V = 11
+    model = make_model(V, V)
+    model.to(device)
+    criterion = LabelSmoothing(smoothing=0.0, vocab_size=V, pad_idx=0)
+    optimizer = NoamOpt(model.parameters(), args.d_model, args.warmup, args.factor)
+    best_loss = float('inf')
+    for ep in range(1000):
+        model.train()
+        train_iter = data_gen(30, 20)
+        run_epoch(ep, train_iter, model, criterion, optimizer)
+
+        if (ep + 1) % EPOCH_CHECK == 0:
+            print("[EVALUATING]")
+            with torch.no_grad():
+                model.eval()
+                dev_loss = run_epoch(ep, data_gen(30, 20), model, criterion, optimizer=None)
+                if dev_loss < best_loss:
+                    best_loss = dev_loss
+                    # save_model(model, args.model_path + "_" + str(ep) + ".tar")
+
+
 if __name__ == "__main__":
     args = argps()
-    train(args)
+    train_synthetic()
--- tests/test_encoder_decoder.py
+++ tests/test_encoder_decoder.py
@@ -21,6 +21,7 @@ class PositionwiseFeedForwardTrue(nn.Module):
     def forward(self, x):
         return self.w_2(self.dropout(F.relu(self.w_1(x))))
 
+
 def attention(query, key, value, mask=None, dropout=None):
     "Compute 'Scaled Dot Product Attention'"
     d_k = query.size(-1)
@@ -28,10 +29,11 @@ def attention(query, key, value, mask=None, dropout=None):
              / math.sqrt(d_k)
     if mask is not None:
         scores = scores.masked_fill(mask == 0, -1e9)
-    p_attn = F.softmax(scores, dim = -1)
+    p_attn = F.softmax(scores, dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)
     return torch.matmul(p_attn, value), p_attn
+
 
 class MultiHeadedAttentionTrue(nn.Module):
     def __init__(self, h, d_model, dropout=0.1):
@@ -66,12 +68,15 @@ class MultiHeadedAttentionTrue(nn.Module):
             .view(nbatches, -1, self.h * self.d_k)
         return self.linears[-1](x)
 
+
 def clones(module, N):
     "Produce N identical layers."
     return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
 
+
 class LayerNorm(nn.Module):
     "Construct a layernorm module (See citation for details)."
+
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
         self.a_2 = nn.Parameter(torch.ones(features))
@@ -83,11 +88,13 @@ class LayerNorm(nn.Module):
         std = x.std(-1, keepdim=True)
         return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
 
+
 class SublayerConnection(nn.Module):
     """
     A residual connection followed by a layer norm.
     Note for code simplicity the norm is first as opposed to last.
     """
+
     def __init__(self, size, dropout):
         super(SublayerConnection, self).__init__()
         self.norm = LayerNorm(size)
@@ -97,8 +104,10 @@ class SublayerConnection(nn.Module):
         "Apply residual connection to any sublayer with the same size."
         return x + self.dropout(sublayer(self.norm(x)))
 
+
 class EncoderLayerTrue(nn.Module):
     "Encoder is made up of self-attn and feed forward (defined below)"
+
     def __init__(self, size, self_attn, feed_forward, dropout):
         super(EncoderLayerTrue, self).__init__()
         self.self_attn = self_attn
@@ -111,9 +120,31 @@ class EncoderLayerTrue(nn.Module):
         x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, mask))
         return self.sublayer[1](x, self.feed_forward)
 
+
 def init_weight(model):
     for p in model.parameters():
-        nn.init.constant_(p, 0.38)
+        torch.manual_seed(0)
+        nn.init.normal_(p, 10, 100)
+
+
+class DecoderLayerTrue(nn.Module):
+    "Decoder is made of self-attn, src-attn, and feed forward (defined below)"
+
+    def __init__(self, size, self_attn, src_attn, feed_forward, dropout):
+        super(DecoderLayerTrue, self).__init__()
+        self.size = size
+        self.self_attn = self_attn
+        self.src_attn = src_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 3)
+
+    def forward(self, x, memory, src_mask, tgt_mask):
+        "Follow Figure 1 (right) for connections."
+        m = memory
+        x = self.sublayer[0](x, lambda x: self.self_attn(x, x, x, tgt_mask))
+        x = self.sublayer[1](x, lambda x: self.src_attn(x, m, m, src_mask))
+        return self.sublayer[2](x, self.feed_forward)
+
 
 class TestModule(unittest.TestCase):
     def test_encoder_layer(self):
@@ -121,7 +152,8 @@ class TestModule(unittest.TestCase):
         myNorm = encoder_decoder.LayerNorm(8)
         myAttention = mul_attention.MultiHeadAttention(head_num=2, hidden_size=8, dropout=0)
         myFF = modules.PositionwiseFeedForward(d_model=8, d_ff=16, dropout_probs=0)
-        myEncoderLayer = encoder_decoder.EncoderLayer(hidden_size=8, self_attn=myAttention, feed_forward=myFF, dropout=0)
+        myEncoderLayer = encoder_decoder.EncoderLayer(hidden_size=8, self_attn=myAttention, feed_forward=myFF,
+                                                      dropout=0)
         init_weight(myEncoderLayer)
 
         torch.manual_seed(0)
@@ -133,9 +165,9 @@ class TestModule(unittest.TestCase):
 
         rand_tensor = torch.randn(3, 10, 8)
         mask = torch.zeros(3, 10).long()
-        mask[0, 0]=1
-        mask[1, 0]=1
-        mask[1, 1]=1
+        mask[0, 0] = 1
+        mask[1, 0] = 1
+        mask[1, 1] = 1
         mask = mask.unsqueeze(1)
         my_norm, true_norm = myNorm(rand_tensor), trueNorm(rand_tensor)
         my_output = myEncoderLayer(rand_tensor, mask)
@@ -146,6 +178,35 @@ class TestModule(unittest.TestCase):
         self.assertTrue(torch.allclose(my_norm, true_norm))
         self.assertTrue(torch.allclose(myFF(rand_tensor), trueFF(rand_tensor)))
         self.assertTrue(torch.allclose(my_output, true_output))
+
+    def test_decoder_layer(self):
+        torch.manual_seed(0)
+        myAttention1 = mul_attention.MultiHeadAttention(head_num=2, hidden_size=8, dropout=0)
+        myAttention2 = mul_attention.MultiHeadAttention(head_num=2, hidden_size=8, dropout=0)
+        myFF = modules.PositionwiseFeedForward(d_model=8, d_ff=16, dropout_probs=0)
+        myDecoderLayer = encoder_decoder.DecoderLayer(hidden_size=8, src_attn=myAttention1, self_attn=myAttention2,
+                                                      feed_forward=myFF,
+                                                      dropout=0)
+        init_weight(myDecoderLayer)
+
+        torch.manual_seed(0)
+        trueAttention1 = MultiHeadedAttentionTrue(2, 8, 0)
+        trueAttention2 = MultiHeadedAttentionTrue(2, 8, 0)
+        trueFF = PositionwiseFeedForwardTrue(8, 16, 0)
+        trueDecoderLayer = DecoderLayerTrue(8, trueAttention1, trueAttention2, trueFF, 0)
+        init_weight(trueDecoderLayer)
+
+        rand_tensor1 = torch.randn(3, 10, 8)
+        rand_tensor2 = torch.rand(3, 10, 8)
+        mask1 = torch.zeros(3, 10).long()
+        mask1[0, 0] = 1
+        mask1[1, 0] = 1
+        mask1[1, 1] = 1
+        mask1 = mask1.unsqueeze(1)
+        my_output = myDecoderLayer(rand_tensor1, rand_tensor2, mask1, mask1)
+        true_output = trueDecoderLayer(rand_tensor1, rand_tensor2, mask1, mask1)
+        self.assertTrue(torch.allclose(my_output, true_output))
+
 
 if __name__ == '__main__':
     unittest.main()
